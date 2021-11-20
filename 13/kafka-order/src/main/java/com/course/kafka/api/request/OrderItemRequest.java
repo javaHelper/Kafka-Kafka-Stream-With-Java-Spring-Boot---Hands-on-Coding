3b59a905de3f6957bede3bee39332c0ee6fa7128
Prateek <prateek.ashtikar512@gmail.com>
2021-11-20 23:28:44 +0530

    Kafka Order

---
+++ 13/kafka-order/src/main/java/com/course/kafka/api/request/OrderItemRequest.java
@@ -0,0 +1,16 @@
+package com.course.kafka.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItemRequest {
+	private String itemName;
+	private int price;
+	private int quantity;
+}
